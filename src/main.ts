37c5f1507c1399655fa12a651456b40f3ee2b7ae
lofcgo1205@gmail.com <lofcgo1205@gmail.com>
2025-05-10 00:58:06 +0900

    [Feat] session, redis 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as session from 'express-session';
+import { createClient } from 'redis';
+import { RedisStore } from 'connect-redis';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
+  // Swagger 설정
   const config = new DocumentBuilder()
     .setTitle('Oauth2 login API')
     .setDescription('NestJS API description')
@@ -18,6 +24,30 @@ async function bootstrap() {
     origin: true, // 추후 프론트엔드 주소로 변경
     credentials: true,
   });
-  await app.listen(process.env.PORT as string);
+
+  // redis 설정
+  const redisClient = createClient({
+    socket: {
+      host: configService.get('redis.host'),
+      port: configService.get('redis.port'),
+    },
+  });
+  await redisClient.connect();
+
+  // session 설정
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      secret: configService.get('session.secret') as string,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30일
+      },
+    }),
+  );
+
+  await app.listen(configService.get('port') as number);
 }
 bootstrap();
