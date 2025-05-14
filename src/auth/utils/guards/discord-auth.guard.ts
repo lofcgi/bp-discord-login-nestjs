97435ea9b1e618dcacea3e48b515f296e8e94edd
lofcgo1205@gmail.com <lofcgo1205@gmail.com>
2025-05-14 11:15:42 +0900

    [Feat] guard 추가

---
+++ src/auth/utils/guards/discord-auth.guard.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return */
+
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class DiscordAuthGuard extends AuthGuard('discord') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const activate = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    return activate;
+  }
+}
